c9067e7a77482df45955dca9f81201df1daf2caf
fluentnumbers <fluentnumbers@gmail.com>
2023-02-24 21:48:33 +0100

    after docker introduction

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -13,54 +13,58 @@ from sqlalchemy import create_engine
 def main(params):
     user = params.user
     password = params.password
-    host = params.host 
-    port = params.port 
+    host = params.host
+    port = params.port
     db = params.db
-    table_name = params.table_name
-    url = params.url
-    
+    table_name = params.table_name[0]
+    table_name1 = params.table_name[1]
+    url = params.url[0]
+    url1 = params.url[1]
+
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
         csv_name = 'output.csv.gz'
     else:
         csv_name = 'output.csv'
-
-    os.system(f"wget {url} -O {csv_name}")
+    os.system(f"wget {url} -O {csv_name} --no-check-certificate")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000,)
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
+    for df in df_iter:
+        t_start = time()
 
-    while True: 
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
+        df.to_sql(name=table_name, con=engine, if_exists='append',)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        t_end = time()
 
-            df.to_sql(name=table_name, con=engine, if_exists='append')
+        print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
-            t_end = time()
 
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
-            break
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url1.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+    os.system(f"wget {url1} -O {csv_name} --no-check-certificate")
+    taxi_zone = pd.read_csv(url1,)
+    taxi_zone.to_sql(name=table_name1,con=engine,if_exists='replace')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
@@ -70,8 +74,8 @@ if __name__ == '__main__':
     parser.add_argument('--host', required=True, help='host for postgres')
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--table_name', nargs=2, required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', nargs=2,required=True, help='url of the csv file')
 
     args = parser.parse_args()
 
