1da2305d0e5fcc8f70e64fc9fd7183b2bb195d45
fluentnumbers <fluentnumbers@gmail.com>
2023-03-05 14:25:36 +0000

    week3

--- week_3_data_warehouse/big_query_hw.sql
+++ week_3_data_warehouse/big_query_hw.sql
@@ -1,30 +1,34 @@
-CREATE OR REPLACE EXTERNAL TABLE `taxi-rides-ny.nytaxi.fhv_tripdata`
+CREATE OR REPLACE EXTERNAL TABLE `trips_data_all.fhv_tripdata`
 OPTIONS (
-  format = 'CSV',
-  uris = ['gs://nyc-tl-data/trip data/fhv_tripdata_2019-*.csv']
+  format = 'parquet',
+  uris = ['gs://dtc_data_lake_dtc-de-333333/data/fhv/fhv_tripdata_2019-*.parquet']
 );
 
-
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
-
-
-SELECT COUNT(DISTINCT(dispatching_base_num)) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+SELECT count(*) FROM `trips_data_all.fhv_tripdata`;
 
 
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-AS SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `trips_data_all.fhv_tripdata`;
 
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-PARTITION BY DATE(dropoff_datetime)
-CLUSTER BY dispatching_base_num AS (
-  SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`
+
+CREATE OR REPLACE TABLE `trips_data_all.fhv_nonpartitioned_tripdata`
+AS SELECT * FROM `trips_data_all.fhv_tripdata`;
+
+
+SELECT count(*) FROM `trips_data_all.fhv_tripdata`
+WHERE PUlocationID is null AND DOlocationID is null
+
+
+CREATE OR REPLACE TABLE `trips_data_all.fhv_partitioned_tripdata`
+PARTITION BY DATE(pickup_datetime)
+CLUSTER BY Affiliated_base_number AS (
+  SELECT * FROM `trips_data_all.fhv_tripdata`
 );
 
-SELECT count(*) FROM  `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM  `trips_data_all.fhv_partitioned_tripdata`
+WHERE pickup_datetime BETWEEN '2019-03-01' AND '2019-03-31'
+  -- AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
 
 
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `trips_data_all.fhv_nonpartitioned_tripdata`
+WHERE pickup_datetime BETWEEN '2019-03-01' AND '2019-03-31'
+  -- AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
